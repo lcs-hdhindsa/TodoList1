dd818aa93bceee2f316bc0ed84c684958f1131f8
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-02-01 16:08:20 -0500

    Initial Commit

---
+++ TodoList1/TodoList1App.swift
@@ -0,0 +1,17 @@
+//
+//  TodoList1App.swift
+//  TodoList1
+//
+//  Created by Harshan Dhindsa on 2025-02-01.
+//
+
+import SwiftUI
+
+@main
+struct TodoList1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
