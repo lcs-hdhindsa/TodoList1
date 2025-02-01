0af21847cc1ab99fcd10cd0644b126adae4d50f0
Harshan Dhindsa <hdhindsa@lcs.on.ca>
2025-02-01 17:21:23 -0500

    Organized Project structure.

--- TodoList1/TodoList1App.swift
+++ TodoList1/TodoList1App.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct TodoList1App: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            LandingView()
         }
     }
 }
--- TodoList1/Views/LandingView.swift
+++ TodoList1/Views/LandingView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  LandingView.swift
 //  TodoList1
 //
 //  Created by Harshan Dhindsa on 2025-02-01.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct LandingView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    LandingView()
 }
